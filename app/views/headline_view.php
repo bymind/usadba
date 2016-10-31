cc95e82fe519ded0cbcfdcee8e6edee32c657fe8
bymind <studio@bymind.ru>
2016-10-31 05:18:40 +0300

    headline edit

--- app/views/headline_view.php
+++ app/views/headline_view.php
@@ -1,7 +1,25 @@
 <div class="container">
-	<div class="headline">
-		<div class="col-xs-2">
-			<div class="logo">
+	<div class="row">
+		<div class="headline clearfix">
+
+			<div class="col-xs-4">
+				<div class="logo">
+				<a href="/"></a>
+				</div>
+				<div class="logo-sitename">
+					<a href="/">Кулинария<br>Усадьба-Центр</a>
+				</div>
+			</div>
+
+			<div class="col-xs-6">
+				<div class="head-phone">
+					<span class="phone-label">
+						Ежедневно с 9 до 23
+					</span>
+					<span class="phone-number">
+						8(800)555-35-35
+					</span>
+				</div>
 			</div>
 		</div>
 	</div>
