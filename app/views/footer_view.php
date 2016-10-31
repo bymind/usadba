31c43a28f2a8b78147231f3648e43492c0bcf045
bymind <studio@bymind.ru>
2016-10-31 15:32:15 +0300

    footer menu deleted

--- app/views/footer_view.php
+++ app/views/footer_view.php
@@ -1,17 +1,4 @@
 <footer>
-	<nav class="main_menu footer_menu container">
-		<ul class="menu_text_units">
-			<li><a href="/about">About</a></li>
-			<li><a href="/news">News</a></li>
-			<li><a href="/shipping-and-payment">Shipping and payment</a></li>
-			<li><a href="/contacts">Contacts</a></li>
-		</ul>
-		
-		<ul class="menu_ui_units">
-			<li><a title="Facebook" href="https://www.fb.com/LaikaProGuitar" target="_blank" class="facebook active"></a></li>
-			<li><a title="Instagram" href="https://www.instagram.com/russiantravelguitar/" target="_blank" class="instagram active"></a></li>
-		</ul>
-	</nav>
 
 	<div class="container-fluid light-grey">
 		<div class="copyright-line">
@@ -28,7 +15,7 @@
 		</div>
 		<div class="row">
 			<div class="col-xs-20">
-				
+
 			</div>
 		</div>
 		</div>
