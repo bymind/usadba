03884e70556d18734a62f99108c440e76166bcb5
bymind <studio@bymind.ru>
2016-10-31 15:35:08 +0300

    Navigation view edit

--- app/views/navigation_view.php
+++ app/views/navigation_view.php
@@ -1,22 +1,19 @@
-<nav class="main_menu">
-	<div class="container">
-		<div class="row">
-			<div class="hidden-xs hidden-sm col-md-12">
+<div class="container">
+	<div class="row">
+		<div class="hidden-xs hidden-sm col-md-12">
+			<nav class="main_menu">
 				<ul class="menu_text_units">
-					<li><a href="/about">About</a></li>
-					<li><a href="/news">News</a></li>
-					<li><a href="/shipping-and-payment">Shipping and payment</a></li>
-					<li><a href="/contacts">Contacts</a></li>
+					<li><a class="arrow pclick" href="/catalog">Каталог</a></li><!--
+					 --><li><a href="/sales">Акции</a></li><!--
+					 --><li><a href="/news">Новости</a></li><!--
+					 --><li><a href="/delivery">Доставка</a></li><!--
+					 --><li><a href="/payment">Оплата</a></li><!--
+					 --><li><a href="/about">О нас</a></li><!--
+					 --><li><a href="/reviews">Отзывы</a></li><!--
+					 --><!-- <li><a class="arrow pclick" href="/else">Еще</a></li> -->
+					<li class="search-item"><input class="search-text" type="text" placeholder="Поиск по каталогу"><button class="btn-search"></button></li>
 				</ul>
-			</div>
-			<div class="col-xs-20 col-md-8">
-				<ul class="menu_ui_units">
-					<li><a href="/eng" title="English" class="active">En</a></li>
-					<li><a href="/ru" title="Русский">Rus</a></li>
-					<li><a title="Facebook" href="https://www.fb.com/LaikaProGuitar" target="_blank" class="facebook active"></a></li>
-					<li><a title="Instagram" href="https://www.instagram.com/russiantravelguitar/" target="_blank" class="instagram active"></a></li>
-				</ul>
-			</div>
+			</nav>
 		</div>
 	</div>
-</nav>
+</div>
