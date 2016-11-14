691285114a783a4cf73d9bba826c919c59f244a4
bymind <studio@bymind.ru>
2016-11-14 14:58:26 +0300

    ебучая навигация

--- app/views/navigation_view.php
+++ app/views/navigation_view.php
@@ -2,21 +2,23 @@
 	<div class="row">
 		<div class="col-xs-12">
 			<nav class="main_menu">
-				<ul class="menu_text_units">
-				<div class="col-xs-10 col-sm-8">
-					<li class="show-li item-li"><a class="arrow pclick" href="/catalog">Каталог</a></li><!--
+				<div class="col-xxs-6 col-xs-7 col-sm-8 p-0">
+				<ul class="menu_text_units"><!--
+					 --><li class="show-li item-li"><a class="arrow pclick" href="/catalog">Каталог</a></li><!--
 					--><li class="show-li item-li"><a href="/sales">Акции</a></li><!--
 					--><li class="show-li item-li"><a href="/news">Новости</a></li><!--
 					--><li class="show-li item-li"><a href="/delivery">Доставка</a></li><!--
 					--><li class="show-li item-li"><a href="/payment">Оплата</a></li><!--
 					--><li class="show-li item-li"><a href="/about">О нас</a></li><!--
 					--><li class="show-li item-li"><a href="/reviews">Отзывы</a></li><!--
-					--><li class="hide-li-else else"><a class="arrow pclick" href="/else">Еще</a></li>
+					--><li class="hide-li-else else"><a class="arrow pclick" href="/#else">Еще</a><ul class="else-ul"></ul></li><!--
+					 --></ul>
 				</div>
-				<div class="col-xs-2 col-sm-4">
-					<li class="search-item"><input class="search-text  hidden-xs" type="text" placeholder="Поиск по каталогу"><button class="btn-search hidden-xs"></button><button class="btn-search mini-search visible-xs"></button></li>
+				<div class="col-xxs-6 col-xs-5 col-sm-4 p-0">
+					<ul class="menu_text_units"><!--
+						 --><li class="search-item"><input class="search-text  hidden-xxs" type="text" placeholder="Поиск по каталогу"><button class="btn-search hidden-xxs"></button><button class="btn-search mini-search visible-xxs"></button></li><!--
+						 					 --></ul>
 				</div>
-				</ul>
 			</nav>
 		</div>
 	</div>
