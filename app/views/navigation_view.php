5c9484f35b752c1c34e96e3678c13962f297940a
bymind <studio@bymind.ru>
2016-11-14 07:13:40 +0300

    Адаптивность и все дела

--- app/views/navigation_view.php
+++ app/views/navigation_view.php
@@ -3,15 +3,19 @@
 		<div class="col-xs-12">
 			<nav class="main_menu">
 				<ul class="menu_text_units">
+				<div class="col-xs-10 col-sm-8">
 					<li class="show-li item-li"><a class="arrow pclick" href="/catalog">Каталог</a></li><!--
-					 --><li class="show-li item-li"><a href="/sales">Акции</a></li><!--
-					 --><li class="show-li item-li"><a href="/news">Новости</a></li><!--
-					 --><li class="show-li item-li"><a href="/delivery">Доставка</a></li><!--
-					 --><li class="show-li item-li"><a href="/payment">Оплата</a></li><!--
-					 --><li class="show-li item-li"><a href="/about">О нас</a></li><!--
-					 --><li class="show-li item-li"><a href="/reviews">Отзывы</a></li><!--
-					 --><li class="hide-li-else else"><a class="arrow pclick" href="/else">Еще</a></li>
-					<li class="search-item"><input class="search-text" type="text" placeholder="Поиск по каталогу"><button class="btn-search"></button></li>
+					--><li class="show-li item-li"><a href="/sales">Акции</a></li><!--
+					--><li class="show-li item-li"><a href="/news">Новости</a></li><!--
+					--><li class="show-li item-li"><a href="/delivery">Доставка</a></li><!--
+					--><li class="show-li item-li"><a href="/payment">Оплата</a></li><!--
+					--><li class="show-li item-li"><a href="/about">О нас</a></li><!--
+					--><li class="show-li item-li"><a href="/reviews">Отзывы</a></li><!--
+					--><li class="hide-li-else else"><a class="arrow pclick" href="/else">Еще</a></li>
+				</div>
+				<div class="col-xs-2 col-sm-4">
+					<li class="search-item"><input class="search-text  hidden-xs" type="text" placeholder="Поиск по каталогу"><button class="btn-search hidden-xs"></button><button class="btn-search mini-search visible-xs"></button></li>
+				</div>
 				</ul>
 			</nav>
 		</div>
