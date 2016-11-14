248c62560ca41f9e8646e903ef99e0a5629be147
bymind <studio@bymind.ru>
2016-11-14 09:24:48 +0300

    Editing -lg- styles for buttons

--- app/views/headline_view.php
+++ app/views/headline_view.php
@@ -19,7 +19,7 @@
 				</div> -->
 			</div>
 
-			<div class="col-xxs-6 col-xas-3 col-xs-3 col-sm-2 col-lg-3">
+			<div class="col-xxs-6 col-xas-3 col-xs-3 col-sm-2 col-lg-2">
 				<div class="head-phone">
 					<span class="phone-label">
 						Ежедневно с 9 до 23
@@ -30,7 +30,7 @@
 				</div>
 			</div>
 
-			<div class="col-xxs-12 col-xas-5 col-xs-6 col-sm-7 col-lg-6 mt-xss-10">
+			<div class="col-xxs-12 col-xas-5 col-xs-6 col-sm-7 col-lg-7 mt-xss-10">
 				<button class="btn-mini col-xxs-4 cart" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Vivamus
 sagittis lacus vel augue laoreet rutrum faucibus.">
 					<span class="counter">0</span>
