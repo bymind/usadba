dbf7bfd11b28e77cb88963848b65e6789078144c
bymind <studio@bymind.ru>
2016-11-14 08:38:27 +0300

    Секс с адаптивным headerline и кнопками

--- app/views/headline_view.php
+++ app/views/headline_view.php
@@ -2,16 +2,24 @@
 	<div class="row">
 		<div class="headline mt-20 mb-20 mt-xs-10 mb-xs-10 clearfix">
 
-			<div class="col-xs-4 col-sm-3">
+			<div class="col-xxs-6 col-xas-4 col-xs-3 col-sm-3">
 				<div class="logo">
 				<a href="/"></a>
 				</div>
 				<div class="logo-sitename">
 					<a href="/">Кулинария<br>Усадьба-Центр</a>
 				</div>
+				<!-- <div class="head-phone under-logo">
+					<span class="phone-label">
+						Ежедневно с 9 до 23
+					</span>
+					<span class="phone-number">
+						8(800)555-35-35
+					</span>
+				</div> -->
 			</div>
 
-			<div class="col-xs-3 col-sm-2 col-lg-3">
+			<div class="col-xxs-6 col-xas-3 col-xs-3 col-sm-2 col-lg-3">
 				<div class="head-phone">
 					<span class="phone-label">
 						Ежедневно с 9 до 23
@@ -22,17 +30,17 @@
 				</div>
 			</div>
 
-			<div class="col-xs-5 col-sm-7 col-lg-6">
-				<button class="btn-wide call-me hidden-xs">
-					<span class="hidden-xs">Заказать<br></span>обратный <br class="visible-xs">звонок
-				</button><button class="btn-wide login">
-					Личный <br class="visible-xs">кабинет
-				</button><button class="btn-mini cart" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Vivamus
+			<div class="col-xxs-12 col-xas-5 col-xs-6 col-sm-7 col-lg-6 mt-xss-10">
+				<button class="btn-mini col-xxs-4 cart" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Vivamus
 sagittis lacus vel augue laoreet rutrum faucibus.">
 					<span class="counter">0</span>
 					<span class="price-counter">
 						<span class="num">0</span> руб
 					</span>
+				</button><button class="btn-wide col-xxs-4 login">
+					Личный <br class="visible-xs">кабинет
+				</button><button class="btn-wide col-xxs-4 call-me visible-xxs">
+					<span class="hidden-xs">Заказать<br></span>обратный <br class="visible-xs">звонок
 				</button>
 			</div>
 		</div>
