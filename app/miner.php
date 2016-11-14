ca9b388b992f42d2d14532bced68c06c8079ab52
bymind <studio@bymind.ru>
2016-11-14 07:25:27 +0300

    Отключил пока кофиг и сессию
    Сессия включается в роутинге

--- app/miner.php
+++ app/miner.php
@@ -1,11 +1,11 @@
 <?php
 
 //TODO move session_start() to any controller
-session_start();
+// session_start();
 
 // include core files (core classes)
 
-require_once 'core/config.php'; // config file
+//require_once 'core/config.php'; // config file
 require_once 'core/model.php'; // models parent class
 require_once 'core/view.php'; // views parent class
 require_once 'core/controller.php'; // controllers parent class
