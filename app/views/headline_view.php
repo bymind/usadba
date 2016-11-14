54faf451234d50584660ab6023e87de9ae3e221d
bymind <studio@bymind.ru>
2016-11-14 07:12:26 +0300

    Adaptive editing

--- app/views/headline_view.php
+++ app/views/headline_view.php
@@ -1,8 +1,8 @@
 <div class="container">
 	<div class="row">
-		<div class="headline mt-20 mb-20 clearfix">
+		<div class="headline mt-20 mb-20 mt-xs-10 mb-xs-10 clearfix">
 
-			<div class="col-xs-3">
+			<div class="col-xs-4 col-sm-3">
 				<div class="logo">
 				<a href="/"></a>
 				</div>
@@ -11,7 +11,7 @@
 				</div>
 			</div>
 
-			<div class="col-xs-3">
+			<div class="col-xs-3 col-sm-2 col-lg-3">
 				<div class="head-phone">
 					<span class="phone-label">
 						Ежедневно с 9 до 23
@@ -22,14 +22,12 @@
 				</div>
 			</div>
 
-			<div class="col-xs-6">
-				<button class="btn-wide call-me">
-					Заказать<br>обратный звонок
-				</button>
-				<button class="btn-wide login">
-					Личный кабинет
-				</button>
-				<button class="btn-mini cart" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Vivamus
+			<div class="col-xs-5 col-sm-7 col-lg-6">
+				<button class="btn-wide call-me hidden-xs">
+					<span class="hidden-xs">Заказать<br></span>обратный <br class="visible-xs">звонок
+				</button><button class="btn-wide login">
+					Личный <br class="visible-xs">кабинет
+				</button><button class="btn-mini cart" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Vivamus
 sagittis lacus vel augue laoreet rutrum faucibus.">
 					<span class="counter">0</span>
 					<span class="price-counter">
