571020c38d4966b36e688ee56d777320a34812dd
bymind <studio@bymind.ru>
2016-11-14 20:46:46 +0300

    подменю

--- app/views/navigation_view.php
+++ app/views/navigation_view.php
@@ -2,22 +2,36 @@
 	<div class="row">
 		<div class="col-xs-12">
 			<nav class="main_menu">
-				<div class="col-xxs-6 col-xs-7 col-sm-8 p-0">
-				<ul class="menu_text_units"><!--
-					 --><li class="show-li item-li"><a class="arrow pclick" href="/catalog">Каталог</a></li><!--
+				<div class="col-xxs-6 col-xs-7 col-sm-8 p-0"><!--
+				 --><ul class="menu_text_units"><!--
+					 --><li class="show-li item-li"><a class="arrow pclick" href="/catalog">Каталог</a><ul class="sub-menu"><!--
+					 --><div class="col-xs-6"><li class="sub-li item-li"><a href="/#">Полуфабрикаты</a><ul class="sub-sub-menu"><li class="sub-sub-li"><a href="/#">Пельмени</a></li><!--
+						  --><li class="sub-sub-li"><a href="/#">Котлеты, колбаски</a></li><!--
+						  --><li class="sub-sub-li"><a href="/#">Блинчики</a></li><!--
+						  --><li class="sub-sub-li"><a href="/#">Вареники</a></li><!--
+						  --><li class="sub-sub-li"><a href="/#">Тесто</a></li></ul></li><!--
+						 --><li class="sub-li item-li"><a href="/#">Выпечка</a><ul class="sub-sub-menu"><li class="sub-sub-li"><a href="/#">Сладкая выпечка</a></li><!--
+						  --><li class="sub-sub-li"><a href="/#">Сытная выпечка</a></li></ul></li>
+					 </div>
+					 <div class="col-xs-6"><!--
+					 --><li class="sub-li item-li"><a href="/#">Кондитерские изделия</a><ul class="sub-sub-menu"><li class="sub-sub-li"><a href="/#">Торты</a></li><!--
+						  --><li class="sub-sub-li"><a href="/#">Пирожные</a></li><!--
+						  --><li class="sub-sub-li"><a href="/#">Эклеры</a></li></ul></li><!--
+				 --></div></ul><!--
+				  --></li><!--
 					--><li class="show-li item-li"><a href="/sales">Акции</a></li><!--
 					--><li class="show-li item-li"><a href="/news">Новости</a></li><!--
 					--><li class="show-li item-li"><a href="/delivery">Доставка</a></li><!--
 					--><li class="show-li item-li"><a href="/payment">Оплата</a></li><!--
 					--><li class="show-li item-li"><a href="/about">О нас</a></li><!--
 					--><li class="show-li item-li"><a href="/reviews">Отзывы</a></li><!--
-					--><li class="hide-li-else else"><a class="arrow pclick" href="/#else">Еще</a><ul class="else-ul"></ul></li><!--
+					--><li class="hide-li-else else"><a class="arrow pclick" href="/#else">Еще</a><ul class="else-ul sub-menu"></ul></li><!--
 					 --></ul>
 				</div>
 				<div class="col-xxs-6 col-xs-5 col-sm-4 p-0">
 					<ul class="menu_text_units"><!--
 						 --><li class="search-item"><input class="search-text  hidden-xxs" type="text" placeholder="Поиск по каталогу"><button class="btn-search hidden-xxs"></button><button class="btn-search mini-search visible-xxs"></button></li><!--
-						 					 --></ul>
+											 --></ul>
 				</div>
 			</nav>
 		</div>
