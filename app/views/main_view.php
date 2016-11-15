56eb12e597b2f409d0129fce495055111418679f
bymind <studio@bymind.ru>
2016-11-15 20:41:11 +0300

    Добавлена карусель и сайд-меню

--- app/views/main_view.php
+++ app/views/main_view.php
@@ -1,3 +1,21 @@
-<div style="position:relative; height:800px; width: 100%;">
+<?php include $actions; ?>
 
+<div class="container mb-50">
+	<div class="row">
+		<div class="col-xs-3">
+			<ul class="side-menu">
+				<li><a href="/catalog/popular"><span class="label popular">хит!</span>Популярное</a></li>
+				<li><a href="/catalog/new"><span class="label new">new!</span>Новинки</a></li>
+				<li><a href="/catalog/sales"><span class="label sales">акция!</span>Акции</a></li>
+				<li><a href="/catalog/polufabrikati" class="arrow arr pclick">Полуфабрикаты</a></li>
+				<li><a href="/catalog/kulinariya" class="arrow arr pclick">Кулинария</a></li>
+				<li><a href="/catalog/vipechka" class="arrow arr pclick">Выпечка</a></li>
+				<li><a href="/catalog/konditerskie-izdeliya" class="arrow arr pclick">Кондитерские изделия</a></li>
+				<li><a class="no-border" href="/catalog/odnorazovaya-posuda">Одноразовая посуда</a></li>
+			</ul>
+		</div>
+		<div class="col-xs-9">
+
+		</div>
+	</div>
 </div>
