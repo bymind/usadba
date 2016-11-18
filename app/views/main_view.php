610d076bc099328a396142dc7b6139c699725eb5
bymind <studio@bymind.ru>
2016-11-18 17:21:30 +0300

    Карточка товара в линии "Новинки"

--- app/views/main_view.php
+++ app/views/main_view.php
@@ -24,7 +24,9 @@
 					<div class="prev disable"></div>
 					<div class="next"></div>
 				</div>
-				<div class="prod-day-img box-img display labeled" data-imgname="salad-day" data-label="new"><a href="/#" class="prod-link" title="Шницель столичный из куриной грудки 740г"></a></div>
+				<div class="prod-day-img box-img display labeled" data-imgname="salad-day" data-label="new"><a href="/#" class="prod-link" title="Шницель столичный из куриной грудки 740г"></a>
+					<div class="heart" data-imgname="salad-day" title="Добавить в избранное"></div>
+				</div>
 				<div class="prod-name"><a href="/#">Шницель столичный из куриной грудки 740г</a></div>
 				<div class="prod-details">диетическое, 217 Ккал</div>
 				<div class="prod-price"><span class="number">249</span> руб</div>
@@ -71,14 +73,16 @@
 			<div class="col-xs-9">
 				<div class="prod-line-outer prod-theme">
 					<div class="prod-card shadow br-2">
-						<div class="prod-day-img box-img display labeled" data-imgname="salad-day" data-label="sales,new,popular"><a href="/#" class="prod-link" title="Шницель столичный из куриной грудки 740г"></a></div>
-						<div class="prod-name"><a href="/#">Шницель столичный из куриной грудки 740г</a></div>
-						<div class="prod-details">диетическое, 217 Ккал</div>
-						<div class="prod-price"><span class="number">249</span> руб</div>
+						<div class="prod-img box-img display labeled" data-imgname="rozan" data-label="new"><a href="/#" class="prod-link" title="Розанчик с яблоком 64г"></a>
+							<div class="heart" data-imgname="rozan" title="Добавить в избранное"></div>
+						</div>
+						<div class="prod-name"><a href="/#">Розанчик с яблоком 64г</a></div>
+						<div class="prod-details">из натуральных яблок, 242 Ккал</div>
+						<div class="prod-price"><span class="number">30</span> руб</div>
 						<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
 						<div class="prod-btn-block">
 							<div class="prod-avail">в наличии</div>
-							<div class="prod-rev">отзывов - <span class="rev">5</span></div>
+							<div class="prod-rev zero">отзывов - <span class="rev">5</span></div>
 							<button class="to-cart">Купить</button>
 						</div>
 					</div>
