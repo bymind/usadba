76c83acd4ec15a906eb40575e311b52b8c8f67b4
bymind <studio@bymind.ru>
2016-11-23 04:50:04 +0300

    новые  блоки
    новости в сайдбаре, сладер популярных, три блока снизу

--- app/views/main_view.php
+++ app/views/main_view.php
@@ -1,11 +1,11 @@
 <?php include $actions; ?>
 
-<div class="container mb-50">
+<div class="container mb-20">
 	<div class="row">
 
 		<div class="col-xxs-12 col-xs-6 col-sm-3">
 
-			<ul class="side-menu shadow mb-20">
+			<ul class="side-menu shadow mb-20 br-2">
 				<li><a href="/catalog/popular"><span class="label popular">хит!</span>Популярное</a></li>
 				<li><a href="/catalog/new"><span class="label new">new!</span>Новинки</a></li>
 				<li><a href="/catalog/sales"><span class="label sales">акция!</span>Акции</a></li>
@@ -16,7 +16,7 @@
 				<li><a class="no-border" href="/catalog/odnorazovaya-posuda">Одноразовая посуда</a></li>
 			</ul>
 
-			<div class="prod-day shadow br-2">
+			<div class="prod-day shadow mb-20 br-2">
 				<div class="prod-day-title">
 					Товар дня
 				</div>
@@ -36,7 +36,22 @@
 					<div class="prod-rev">отзывов - <span class="rev">5</span></div>
 					<button class="to-cart">Купить</button>
 				</div>
+			</div>
 
+			<div class="side-news shadow br-2">
+				<div class="side-news-title">
+					Новости
+				</div>
+				<div class="arr">
+					<div class="prev disable"></div>
+					<div class="next"></div>
+				</div>
+				<div class="news-img box-img display"><a href="/#" class="news-link" title="Мы открываем доставку в область!">
+					<img src="img/news/boobs.jpg" alt="Мы открываем доставку в область!">
+				</a>
+				</div>
+				<div class="news-title"><a href="/#">Мы открываем доставку в область!</a></div>
+				<div class="news-anons">По многочисленным просьбам наших покупателей мы приняли решение доставлять за пределы МКАД. <a class="more" href="/#">подробнее</a></div>
 			</div>
 
 		</div>
@@ -66,12 +81,12 @@
 			</div>
 
 			<div class="col-xs-9">
-				<div class="title-wide mb-20">
+				<div class="title-wide mb-10">
 					Новинки
 				</div>
 			</div>
-			<div class="col-xs-9">
-				<div class="prod-line-outer prod-theme">
+			<div class="col-xs-9 mb-20">
+				<div class="prod-line-outer prod-new prod-theme">
 					<div class="prod-card shadow br-2">
 						<div class="prod-img box-img display labeled" data-imgname="rozan" data-label="new"><a href="/#" class="prod-link" title="Розанчик с яблоком 64г"></a>
 							<div class="heart" data-imgname="rozan" title="Добавить в избранное"></div>
@@ -87,31 +102,280 @@
 						</div>
 					</div>
 					<div class="prod-card shadow br-2">
-						<h1>2</h1>
+						<div class="prod-img box-img display labeled" data-imgname="rozan" data-label="new"><a href="/#" class="prod-link" title="Розанчик с яблоком 64г"></a>
+							<div class="heart" data-imgname="rozan" title="Добавить в избранное"></div>
+						</div>
+						<div class="prod-name"><a href="/#">Розанчик с яблоком 64г</a></div>
+						<div class="prod-details">из натуральных яблок, 242 Ккал</div>
+						<div class="prod-price"><span class="number">30</span> руб</div>
+						<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
+						<div class="prod-btn-block">
+							<div class="prod-avail">в наличии</div>
+							<div class="prod-rev zero">отзывов - <span class="rev">5</span></div>
+							<button class="to-cart">Купить</button>
+						</div>
 					</div>
 					<div class="prod-card shadow br-2">
-						<h1>3</h1>
+						<div class="prod-img box-img display labeled" data-imgname="rozan" data-label="new"><a href="/#" class="prod-link" title="Розанчик с яблоком 64г"></a>
+							<div class="heart" data-imgname="rozan" title="Добавить в избранное"></div>
+						</div>
+						<div class="prod-name"><a href="/#">Розанчик с яблоком 64г</a></div>
+						<div class="prod-details">из натуральных яблок, 242 Ккал</div>
+						<div class="prod-price"><span class="number">30</span> руб</div>
+						<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
+						<div class="prod-btn-block">
+							<div class="prod-avail">в наличии</div>
+							<div class="prod-rev zero">отзывов - <span class="rev">5</span></div>
+							<button class="to-cart">Купить</button>
+						</div>
 					</div>
 					<div class="prod-card shadow br-2">
-						<h1>4</h1>
+						<div class="prod-img box-img display labeled" data-imgname="rozan" data-label="new"><a href="/#" class="prod-link" title="Розанчик с яблоком 64г"></a>
+							<div class="heart" data-imgname="rozan" title="Добавить в избранное"></div>
+						</div>
+						<div class="prod-name"><a href="/#">Розанчик с яблоком 64г</a></div>
+						<div class="prod-details">из натуральных яблок, 242 Ккал</div>
+						<div class="prod-price"><span class="number">30</span> руб</div>
+						<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
+						<div class="prod-btn-block">
+							<div class="prod-avail">в наличии</div>
+							<div class="prod-rev zero">отзывов - <span class="rev">5</span></div>
+							<button class="to-cart">Купить</button>
+						</div>
 					</div>
 					<div class="prod-card shadow br-2">
-						<h1>5</h1>
+						<div class="prod-img box-img display labeled" data-imgname="rozan" data-label="new"><a href="/#" class="prod-link" title="Розанчик с яблоком 64г"></a>
+							<div class="heart" data-imgname="rozan" title="Добавить в избранное"></div>
+						</div>
+						<div class="prod-name"><a href="/#">Розанчик с яблоком 64г</a></div>
+						<div class="prod-details">из натуральных яблок, 242 Ккал</div>
+						<div class="prod-price"><span class="number">30</span> руб</div>
+						<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
+						<div class="prod-btn-block">
+							<div class="prod-avail">в наличии</div>
+							<div class="prod-rev zero">отзывов - <span class="rev">5</span></div>
+							<button class="to-cart">Купить</button>
+						</div>
 					</div>
 					<div class="prod-card shadow br-2">
-						<h1>6</h1>
+						<div class="prod-img box-img display labeled" data-imgname="rozan" data-label="new"><a href="/#" class="prod-link" title="Розанчик с яблоком 64г"></a>
+							<div class="heart" data-imgname="rozan" title="Добавить в избранное"></div>
+						</div>
+						<div class="prod-name"><a href="/#">Розанчик с яблоком 64г</a></div>
+						<div class="prod-details">из натуральных яблок, 242 Ккал</div>
+						<div class="prod-price"><span class="number">30</span> руб</div>
+						<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
+						<div class="prod-btn-block">
+							<div class="prod-avail">в наличии</div>
+							<div class="prod-rev zero">отзывов - <span class="rev">5</span></div>
+							<button class="to-cart">Купить</button>
+						</div>
 					</div>
 					<div class="prod-card shadow br-2">
-						<h1>7</h1>
+						<div class="prod-img box-img display labeled" data-imgname="rozan" data-label="new"><a href="/#" class="prod-link" title="Розанчик с яблоком 64г"></a>
+							<div class="heart" data-imgname="rozan" title="Добавить в избранное"></div>
+						</div>
+						<div class="prod-name"><a href="/#">Розанчик с яблоком 64г</a></div>
+						<div class="prod-details">из натуральных яблок, 242 Ккал</div>
+						<div class="prod-price"><span class="number">30</span> руб</div>
+						<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
+						<div class="prod-btn-block">
+							<div class="prod-avail">в наличии</div>
+							<div class="prod-rev zero">отзывов - <span class="rev">5</span></div>
+							<button class="to-cart">Купить</button>
+						</div>
 					</div>
 					<div class="prod-card shadow br-2">
-						<h1>8</h1>
+						<div class="prod-img box-img display labeled" data-imgname="rozan" data-label="new"><a href="/#" class="prod-link" title="Розанчик с яблоком 64г"></a>
+							<div class="heart" data-imgname="rozan" title="Добавить в избранное"></div>
+						</div>
+						<div class="prod-name"><a href="/#">Розанчик с яблоком 64г</a></div>
+						<div class="prod-details">из натуральных яблок, 242 Ккал</div>
+						<div class="prod-price"><span class="number">30</span> руб</div>
+						<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
+						<div class="prod-btn-block">
+							<div class="prod-avail">в наличии</div>
+							<div class="prod-rev zero">отзывов - <span class="rev">5</span></div>
+							<button class="to-cart">Купить</button>
+						</div>
 					</div>
 					<div class="prod-card shadow br-2">
-						<h1>9</h1>
+						<div class="prod-img box-img display labeled" data-imgname="rozan" data-label="new"><a href="/#" class="prod-link" title="Розанчик с яблоком 64г"></a>
+							<div class="heart" data-imgname="rozan" title="Добавить в избранное"></div>
+						</div>
+						<div class="prod-name"><a href="/#">Розанчик с яблоком 64г</a></div>
+						<div class="prod-details">из натуральных яблок, 242 Ккал</div>
+						<div class="prod-price"><span class="number">30</span> руб</div>
+						<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
+						<div class="prod-btn-block">
+							<div class="prod-avail">в наличии</div>
+							<div class="prod-rev zero">отзывов - <span class="rev">5</span></div>
+							<button class="to-cart">Купить</button>
+						</div>
 					</div>
 				</div>
 			</div>
 
+			<div class="col-xs-9">
+				<div class="title-wide mb-10">
+					Самые популярные
+				</div>
+			</div>
+			<div class="col-xs-9 mb-20">
+				<div class="prod-line-outer prod-popular prod-theme">
+					<div class="prod-card shadow br-2">
+						<div class="prod-img box-img display labeled" data-imgname="rozan" data-label="popular"><a href="/#" class="prod-link" title="Розанчик с яблоком 64г"></a>
+							<div class="heart" data-imgname="rozan" title="Добавить в избранное"></div>
+						</div>
+						<div class="prod-name"><a href="/#">Розанчик с яблоком 64г</a></div>
+						<div class="prod-details">из натуральных яблок, 242 Ккал</div>
+						<div class="prod-price"><span class="number">30</span> руб</div>
+						<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
+						<div class="prod-btn-block">
+							<div class="prod-avail">в наличии</div>
+							<div class="prod-rev zero">отзывов - <span class="rev">5</span></div>
+							<button class="to-cart">Купить</button>
+						</div>
+					</div>
+					<div class="prod-card shadow br-2">
+						<div class="prod-img box-img display labeled" data-imgname="rozan" data-label="popular"><a href="/#" class="prod-link" title="Розанчик с яблоком 64г"></a>
+							<div class="heart" data-imgname="rozan" title="Добавить в избранное"></div>
+						</div>
+						<div class="prod-name"><a href="/#">Розанчик с яблоком 64г</a></div>
+						<div class="prod-details">из натуральных яблок, 242 Ккал</div>
+						<div class="prod-price"><span class="number">30</span> руб</div>
+						<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
+						<div class="prod-btn-block">
+							<div class="prod-avail">в наличии</div>
+							<div class="prod-rev zero">отзывов - <span class="rev">5</span></div>
+							<button class="to-cart">Купить</button>
+						</div>
+					</div>
+					<div class="prod-card shadow br-2">
+						<div class="prod-img box-img display labeled" data-imgname="rozan" data-label="popular"><a href="/#" class="prod-link" title="Розанчик с яблоком 64г"></a>
+							<div class="heart" data-imgname="rozan" title="Добавить в избранное"></div>
+						</div>
+						<div class="prod-name"><a href="/#">Розанчик с яблоком 64г</a></div>
+						<div class="prod-details">из натуральных яблок, 242 Ккал</div>
+						<div class="prod-price"><span class="number">30</span> руб</div>
+						<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
+						<div class="prod-btn-block">
+							<div class="prod-avail">в наличии</div>
+							<div class="prod-rev zero">отзывов - <span class="rev">5</span></div>
+							<button class="to-cart">Купить</button>
+						</div>
+					</div>
+					<div class="prod-card shadow br-2">
+						<div class="prod-img box-img display labeled" data-imgname="rozan" data-label="popular"><a href="/#" class="prod-link" title="Розанчик с яблоком 64г"></a>
+							<div class="heart" data-imgname="rozan" title="Добавить в избранное"></div>
+						</div>
+						<div class="prod-name"><a href="/#">Розанчик с яблоком 64г</a></div>
+						<div class="prod-details">из натуральных яблок, 242 Ккал</div>
+						<div class="prod-price"><span class="number">30</span> руб</div>
+						<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
+						<div class="prod-btn-block">
+							<div class="prod-avail">в наличии</div>
+							<div class="prod-rev zero">отзывов - <span class="rev">5</span></div>
+							<button class="to-cart">Купить</button>
+						</div>
+					</div>
+					<div class="prod-card shadow br-2">
+						<div class="prod-img box-img display labeled" data-imgname="rozan" data-label="popular"><a href="/#" class="prod-link" title="Розанчик с яблоком 64г"></a>
+							<div class="heart" data-imgname="rozan" title="Добавить в избранное"></div>
+						</div>
+						<div class="prod-name"><a href="/#">Розанчик с яблоком 64г</a></div>
+						<div class="prod-details">из натуральных яблок, 242 Ккал</div>
+						<div class="prod-price"><span class="number">30</span> руб</div>
+						<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
+						<div class="prod-btn-block">
+							<div class="prod-avail">в наличии</div>
+							<div class="prod-rev zero">отзывов - <span class="rev">5</span></div>
+							<button class="to-cart">Купить</button>
+						</div>
+					</div>
+					<div class="prod-card shadow br-2">
+						<div class="prod-img box-img display labeled" data-imgname="rozan" data-label="popular"><a href="/#" class="prod-link" title="Розанчик с яблоком 64г"></a>
+							<div class="heart" data-imgname="rozan" title="Добавить в избранное"></div>
+						</div>
+						<div class="prod-name"><a href="/#">Розанчик с яблоком 64г</a></div>
+						<div class="prod-details">из натуральных яблок, 242 Ккал</div>
+						<div class="prod-price"><span class="number">30</span> руб</div>
+						<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
+						<div class="prod-btn-block">
+							<div class="prod-avail">в наличии</div>
+							<div class="prod-rev zero">отзывов - <span class="rev">5</span></div>
+							<button class="to-cart">Купить</button>
+						</div>
+					</div>
+					<div class="prod-card shadow br-2">
+						<div class="prod-img box-img display labeled" data-imgname="rozan" data-label="popular"><a href="/#" class="prod-link" title="Розанчик с яблоком 64г"></a>
+							<div class="heart" data-imgname="rozan" title="Добавить в избранное"></div>
+						</div>
+						<div class="prod-name"><a href="/#">Розанчик с яблоком 64г</a></div>
+						<div class="prod-details">из натуральных яблок, 242 Ккал</div>
+						<div class="prod-price"><span class="number">30</span> руб</div>
+						<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
+						<div class="prod-btn-block">
+							<div class="prod-avail">в наличии</div>
+							<div class="prod-rev zero">отзывов - <span class="rev">5</span></div>
+							<button class="to-cart">Купить</button>
+						</div>
+					</div>
+					<div class="prod-card shadow br-2">
+						<div class="prod-img box-img display labeled" data-imgname="rozan" data-label="popular"><a href="/#" class="prod-link" title="Розанчик с яблоком 64г"></a>
+							<div class="heart" data-imgname="rozan" title="Добавить в избранное"></div>
+						</div>
+						<div class="prod-name"><a href="/#">Розанчик с яблоком 64г</a></div>
+						<div class="prod-details">из натуральных яблок, 242 Ккал</div>
+						<div class="prod-price"><span class="number">30</span> руб</div>
+						<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
+						<div class="prod-btn-block">
+							<div class="prod-avail">в наличии</div>
+							<div class="prod-rev zero">отзывов - <span class="rev">5</span></div>
+							<button class="to-cart">Купить</button>
+						</div>
+					</div>
+					<div class="prod-card shadow br-2">
+						<div class="prod-img box-img display labeled" data-imgname="rozan" data-label="popular"><a href="/#" class="prod-link" title="Розанчик с яблоком 64г"></a>
+							<div class="heart" data-imgname="rozan" title="Добавить в избранное"></div>
+						</div>
+						<div class="prod-name"><a href="/#">Розанчик с яблоком 64г</a></div>
+						<div class="prod-details">из натуральных яблок, 242 Ккал</div>
+						<div class="prod-price"><span class="number">30</span> руб</div>
+						<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
+						<div class="prod-btn-block">
+							<div class="prod-avail">в наличии</div>
+							<div class="prod-rev zero">отзывов - <span class="rev">5</span></div>
+							<button class="to-cart">Купить</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+	</div>
+</div>
+
+<div class="container mb-50">
+	<div class="row">
+		<div class="col-xs-4">
+			<div class="shadow br-2 bt-cat news">
+				<div class="img"></div>
+				<div class="title">Наши новости</div>
+			</div>
 		</div>
+
+		<div class="col-xs-4">
+			<div class="shadow br-2 bt-cat sales">
+				<div class="img"></div>
+				<div class="title">Все акции</div>
+			</div>
+		</div>
+
+		<div class="col-xs-4">
+			<div class="shadow br-2 bt-cat delivery">
+				<div class="img"></div>
+				<div class="title">Доставка</div>
+			</div>
+		</div>
+	</div>
 </div>
