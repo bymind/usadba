9b1bf0ebb2d1f320e7e119693256841c07224462
bymind <studio@bymind.ru>
2016-11-25 06:31:30 +0300

    новый футер

--- app/views/footer_view.php
+++ app/views/footer_view.php
@@ -1,13 +1,58 @@
 <footer>
 
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="footer-links-box shadow br-2">
+					<div class="row">
+						<div class="col-xxs-6 col-xas-6 col-xs-6 col-sm-3">
+							<div class="footer-link">
+								<span class="link"><a href="/#">О нас</a></span>
+								<span class="link"><a href="/#">Отзывы</a></span>
+								<span class="link"><a href="/#">Наши новости</a></span>
+								<span class="link"><a href="/#">Карта сайта</a></span>
+								<span class="link"><a href="/#">Контактная информация</a></span>
+							</div>
+						</div>
+						<div class="col-xxs-6 col-xas-6 col-xs-6 col-sm-3">
+							<div class="footer-link">
+								<span class="link"><a href="/#">Условия доставки</a></span>
+								<span class="link"><a href="/#">Способы оплаты</a></span>
+								<span class="link"><a href="/#">Наши гарантии</a></span>
+								<span class="link"><a href="/#">Действующие акции</a></span>
+								<span class="link"><a href="/#">Оптовым клиентам</a></span>
+							</div>
+						</div>
+						<div class="col-xxs-12 hidden-xs col-xs-12 col-sm-4 col-sm-push-2 col-lg-3 col-lg-push-3">
+							<div class="footer-contacts">
+								<div class="title">
+									Контактная информация
+								</div>
+								<div class="addr mb-10">
+									125009, г. Москва,<br>Вознесенский переулок, д. 20, стр. 2<br>ОАО "УСАДЬБА-ЦЕНТР"
+								</div>
+								<div class="phone mb-10">
+									<a href="/#" class="footer-phone">8(800)555-35-35</a>
+								</div>
+								<div class="terms">
+									<a href="/#">Пользовательское соглашение</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
 	<div class="container-fluid light-grey">
 		<div class="copyright-line">
 		<div class="row">
 			<div class="col-xs-20">
 				<span class="copyright">
-					©2016 Laika.pro All Rights Reserved.
+					2016 © Усадьба-Кулинария.ру - доставка полуфабрикатов и кулинарии.
 				</span>
-				<span class="ref">
+				<span class="ref hidden-xs">
 					<a href="http://bymind.ru" title="Designed and developed ByMind.ru" target="_blank">Designed and developed</a>
 					<a class="logo" href="http://bymind.ru" title="Designed and developed ByMind.ru" target="_blank"></a>
 				</span>
