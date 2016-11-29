da36d55acb57f682c25ba93b1db2af03cb3a8b2d
bymind <studio@bymind.ru>
2016-11-30 00:20:18 +0300

    FIX legacy code and load small pict for sales slider

--- js/main_page.js
+++ js/main_page.js
@@ -4,8 +4,6 @@
 */
 
 $(function() {
-	VClickInit();
-	BtnClickInit();
 	InitOwlCarousel();
 	$(window).resize(function(event) {
 		InitOwlCarousel();
@@ -13,90 +11,6 @@ $(function() {
 	CatImageInit();
 });
 
-
-/*
-* VClickInit()
-* Initialisation click to the cover-image of video;
-* @return 0
-*/
-function VClickInit() {
-	$(document).on('click', '.video-plumb', function(event) {
-		VClickHandler($(this));
-	});
-	return 0;
-}
-
-
-/*
-* VClickHandler(obj)
-* Handler click to the cover-image of video;
-* @param obj
-* @return 0
-*/
-function VClickHandler(obj) {
-	var vStrBegin = '<iframe height="100%" width="100%" src="https://www.youtube.com/embed/';
-	var vStrEnd = '?autoplay=1" frameborder="0" allowfullscreen></iframe>';
-	var vStr = vStrBegin + obj.data().videoId + vStrEnd;
-	var vContainer = obj.parent();
-	vContainer.append(vStr);
-	obj.remove();
-	return 0;
-}
-
-
-/*
-* BtnClickInit()
-* Initialisation click to the button;
-* @return 0
-*/
-function BtnClickInit() {
-	$(document).on('click', 'button', function(event) {
-		BtnClickHandler($(this));
-	});
-	return 0;
-}
-
-
-/*
-* BtnClickHandler(obj)
-* Handler click to the button;
-* @param obj
-* @return 0
-*/
-function BtnClickHandler(obj) {
-	var target = obj.data().target;
-
-	switch(target) {
-
-		case 'preorder':
-			ModalPreorderGo(obj);
-			break;
-
-		default:
-			break;
-	}
-
-	return 0;
-}
-
-
-/*
-* ModalPreorderGo(obj)
-* Modal window for preorder
-* @param obj
-* @return 0
-*/
-function ModalPreorderGo(obj) {
-
-	var target = obj.data().target;
-	var mObj = $('#'+target);
-
-	mObj.modal();
-
-	console.log('Hello, modal!');
-
-	return 0;
-}
 
 /*
 * initOwlCarousel()
@@ -115,10 +29,17 @@ function InitOwlCarousel() {
 // считаем отступ для стрелок слайдера
 	arrowsTop = Math.floor((countH-100)/2);
 
+// посмотрим на ширину экрана, если маленькая - то загружаем мини-картинки
+	deviceW = $(document).width();
+
 // добавляем изображения
 	for (var i = $('#sales-carousel .item').length - 1; i >= 0; i--) {
 		curItem = $('#sales-carousel .item:eq('+i+') a');
-		jpgName = "img/sales/" + curItem.data('saleid') + ".jpg";
+		if (deviceW <= 600) {
+			jpgName = "img/sales/" + curItem.data('saleid') + "-min.jpg";
+		} else {
+			jpgName = "img/sales/" + curItem.data('saleid') + ".jpg";
+		}
 		curItem.css({
 			'background-image': 'url('+jpgName+')'
 		});
