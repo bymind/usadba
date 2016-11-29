7697f89763e3f973c894696513fa12a5117744db
bymind <studio@bymind.ru>
2016-11-30 00:03:49 +0300

    manifest editing

--- app/views/favicons.php
+++ app/views/favicons.php
@@ -13,6 +13,9 @@
 <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
 <link rel="icon" type="image/ico" sizes="16x16" href="favicon/favicon.ico">
 <link rel="manifest" href="favicon/manifest.json">
-<meta name="msapplication-TileColor" content="#14767f">
 <meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
+<!-- <meta name="msapplication-TileColor" content="#14767f">
 <meta name="theme-color" content="#14767f">
+ -->
+ <meta name="theme-color" content="#c63838">
+<meta name="msapplication-TileColor" content="#c63838">
