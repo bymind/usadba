4439984018e75262a07ef745097bba241c8614a3
bymind <studio@bymind.ru>
2016-11-30 00:09:56 +0300

    NEW блок новостей для сайдбара

--- app/views/side_news_view.php
+++ app/views/side_news_view.php
@@ -1,4 +1,4 @@
-<div class="side-news shadow br-2 mb-20">
+<div class="hidden-xas side-news shadow br-2 mb-20">
 	<div class="side-news-title">
 		Новости
 	</div>
@@ -6,10 +6,10 @@
 		<div class="prev disable"></div>
 		<div class="next"></div>
 	</div>
-	<div class="news-img box-img display"><a href="/#" class="news-link" title="Мы открываем доставку в область!">
+	<div class="news-img box-img display"><a href="/news/mi-otkrivaem-dostavku-v-oblast" class="news-link" title="Мы открываем доставку в область!">
 		<img src="img/news/boobs.jpg" alt="Мы открываем доставку в область!">
 	</a>
 	</div>
-	<div class="news-title"><a href="/#">Мы открываем доставку в область!</a></div>
-	<div class="news-anons">По многочисленным просьбам наших покупателей мы приняли решение доставлять за пределы МКАД. <a class="more" href="/#">подробнее</a></div>
+	<div class="news-title"><a href="/news/mi-otkrivaem-dostavku-v-oblast">Мы открываем доставку в область!</a></div>
+	<div class="news-anons">По многочисленным просьбам наших покупателей мы приняли решение доставлять за пределы МКАД. <a class="more" href="/news/mi-otkrivaem-dostavku-v-oblast">подробнее</a></div>
 </div>
