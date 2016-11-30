08105ac4354ccdc93656cd1d26e05beaa9dc1fbc
bymind <studio@bymind.ru>
2016-11-30 08:16:12 +0300

    FIX ширина блоков кнопок на вкладках в мод окне логина

--- app/views/modal_profile_view.php
+++ app/views/modal_profile_view.php
@@ -23,7 +23,7 @@
 				</div>
 			</div>
 			<div class="modal-footer login-footer">
-				<div class="col-xxs-12 col-xxs-offset-0 col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2">
+				<div class="col-xxs-12 col-xxs-offset-0 col-xs-8 col-xs-offset-2">
 					<div class="col-xs-5">
 						<button type="button" class="br-2 btn btn-default" data-dismiss="modal">Отмена</button>
 					</div>
