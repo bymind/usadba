9d83fefaf7bab32bdc8be907f39db0588beb90e7
bymind <studio@bymind.ru>
2016-12-04 21:51:36 +0300

    🆕 NEW модальное окно для корзины

---
+++ app/views/modal_cart_view.php
@@ -0,0 +1,31 @@
+<div class="modal fade modal-cart" tabindex="-1" role="dialog">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content br-2">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"></span></button>
+				<h4 class="modal-title only">Корзина</h4>
+			</div>
+			<div class="modal-body">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-xs-10 col-xs-offset-1">
+							<div class="cart-body">
+								<div class="nothing">Тут пока ничего нет.</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<div class="col-xxs-12 col-xxs-offset-0 col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2">
+					<div class="col-xs-5">
+						<button type="button" class="br-2 btn btn-default" data-dismiss="modal">Отмена</button>
+					</div>
+					<div class="col-xs-7">
+						<button type="button" class="br-2 btn btn-primary">Перезвоните мне</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
