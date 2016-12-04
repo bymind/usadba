a7c23032f58bb7ed9dd7b4d23ca096bdba1db43b
bymind <studio@bymind.ru>
2016-12-04 21:52:16 +0300

    ⚠️ FIX продукт дня обернул в .prod-card

--- app/views/side_prod_of_day_view.php
+++ app/views/side_prod_of_day_view.php
@@ -6,16 +6,18 @@
 		<div class="prev disable"></div>
 		<div class="next"></div>
 	</div>
-	<div class="prod-day-img box-img display labeled" data-imgname="salad-day" data-label="new"><a href="/catalog/polufabrikati/shicel-stolichnyi-740g" class="prod-link" title="Шницель столичный из куриной грудки 740г"></a>
-		<div class="heart" data-imgname="salad-day" title="Добавить в избранное"></div>
-	</div>
-	<div class="prod-name"><a href="/catalog/polufabrikati/shicel-stolichnyi-740g">Шницель столичный из куриной грудки 740г</a></div>
-	<div class="prod-details">диетическое, 217 Ккал</div>
-	<div class="prod-price"><span class="number">249</span> руб</div>
-	<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
-	<div class="prod-btn-block">
-		<div class="prod-avail">в наличии</div>
-		<div class="prod-rev">отзывов - <span class="rev">5</span></div>
-		<button class="to-cart">Купить</button>
+	<div data-art="1322" class="prod-card">
+		<div class="prod-day-img box-img display labeled" data-imgname="salad-day" data-label="new"><a href="/catalog/polufabrikati/shicel-stolichnyi-740g" class="prod-link" title="Шницель столичный из куриной грудки 740г"></a>
+			<div class="heart" data-imgname="salad-day" title="Добавить в избранное"></div>
+		</div>
+		<div class="prod-name"><a href="/catalog/polufabrikati/shicel-stolichnyi-740g">Шницель столичный из куриной грудки 740г</a></div>
+		<div class="prod-details">диетическое, 217 Ккал</div>
+		<div class="prod-price"><span class="number">249</span> руб</div>
+		<div class="prod-counts">(<span class="number">1</span> <span class="measure">шт</span>)</div>
+		<div class="prod-btn-block">
+			<div class="prod-avail">в наличии</div>
+			<div class="prod-rev">отзывов - <span class="rev">5</span></div>
+			<button class="to-cart">Купить</button>
+		</div>
 	</div>
 </div>
