1884915312f0deba9625c97d65a848ee0cdec351
bymind <studio@bymind.ru>
2016-12-28 04:37:16 +0300

    FIX disabled buttons if cart is empty

--- app/views/modal_cart_view.php
+++ app/views/modal_cart_view.php
@@ -8,10 +8,12 @@
 			<div class="modal-body">
 				<div class="container-fluid">
 					<div class="row">
-						<div class="col-xs-10 col-xs-offset-1">
+						<div class="col-xs-12">
+						<div class="row">
 							<div class="cart-body">
 								<div class="nothing">Тут пока ничего нет.</div>
 							</div>
+						</div>
 						</div>
 					</div>
 				</div>
@@ -19,10 +21,10 @@
 			<div class="modal-footer">
 				<div class="col-xxs-12 col-xxs-offset-0 col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2">
 					<div class="col-xxs-5 col-xs-6">
-						<button type="button" class="br-2 btn btn-default">Просмотр</button>
+						<button type="button" class="br-2 btn btn-default" disabled="disabled">Просмотр</button>
 					</div>
 					<div class="col-xxs-7 col-xs-6">
-						<button type="button" class="br-2 btn btn-primary">Оформить заказ</button>
+						<button type="button" class="br-2 btn btn-primary" disabled="disabled">Оформить заказ</button>
 					</div>
 				</div>
 			</div>
