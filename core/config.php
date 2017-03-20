381fbfe3c0ba054804b11367db1863444c9633e5
Kesha <studio@bymind.ru>
2017-03-20 20:34:43 +0300

    Add files via upload

---
+++ core/config.php
@@ -0,0 +1,28 @@
+<?php
+
+// DB config
+
+/*$dbHost='a103460.mysql.mchost.ru';
+$dbName='a103460_usadba';
+$dbUser='a103460_usadba';
+$dbPass='usadba';
+*/
+
+$dbHost='localhost';
+$dbName='usadba';
+$dbUser='localhost';
+$dbPass='';
+
+// DB connection
+$db = mysql_connect($dbHost,$dbUser,$dbPass);
+
+// report
+if (!$db) { echo ("Не могу подключиться к серверу базы данных!"); }
+//TODO rewrite with catch
+
+// DB selection
+mysql_select_db($dbName,$db);
+
+// DB charset configuration
+mysql_query("SET NAMES utf8");
+
