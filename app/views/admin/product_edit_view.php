0a72a55fbc6a56a53d888844576841cce37a640e
bymind <keshapudelev@ya.ru>
2017-03-24 02:50:57 +0300

    FIX remove bad sidebar element

--- app/views/admin/product_edit_view.php
+++ app/views/admin/product_edit_view.php
@@ -68,41 +68,7 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-md-9">
-				<div class="settings tags" style="height:auto;">
-					<div class="title ">Рефер в тело статьи<div class='reset'></div></div>
-					<div class="items" style="height:auto">
-						<textarea name="" id="" style="font-size:10px; font-family: monospace; color: rgba(255,255,255,.7); background: #111; border:0;width:185px; max-width:185px; height: 160px;">
-<div class="refer">
-<h2>Заголовок [Стоимость %услуга% в сервисном центре Macfix]</h2>
-<div class="table-responsive">
-<table class="table table-hover upgrade__table">
-<thead>
-<tr>
-<th>Param 1</th>
-<th>Param 2</th>
-<th>Param 3</th>
-<th>Param 4</th>
-<th>Param 5</th>
-</tr>
-</thead>
-<tbody>
-<tr>
-<td>Value 1</td>
-<td>Value 2</td>
-<td>Value 3</td>
-<td>Value 4</td>
-<td>Value 5</td>
-</tr>
-</tbody>
-</table>
-</div>
-<p class="refin" style="margin: 0 !important;"><span class="btns p"><button class="btn_green" data-attr="curier">Вызвать курьера</button><button class="btn_blue article_call" data-attr="call">Позвонить</button></span> <span class="ref_desc">Посмотрите нашу страницу про <a href="/prices/upgrade">апгрейд MacBook</a>.</span></p>
-</div>
-						</textarea>
-					</div>
-				</div>
-			</div>
+
 		</div>
 
 	</div>
