4fb4c55b50b88b1525103eb4efe071b6cdf1abaf
bymind <keshapudelev@ya.ru>
2017-03-23 05:58:21 +0300

    FIX replace articles with goods

--- app/views/admin/navigation_view.php
+++ app/views/admin/navigation_view.php
@@ -62,7 +62,7 @@
 <div class="navigation">
 	<ul class='side-nav' attr-active="<?php echo $active_menu_item; ?> ">
 		<li class="home"><a href="/admin" title="Рабочий стол"><span>Рабочий стол</span></a></li>
-		<li class="articles"><a href="/admin/goods" title="Товары"><span>Товары</span></a></li>
+		<li class="goods"><a href="/admin/goods" title="Товары"><span>Товары</span></a></li>
 		<li class="pages"><a href="/admin/pages"  title="Страницы сайта"><span>Страницы сайта</span></a></li>
 		<li class="files"><a href="/admin/files" title="Файлы"><span>Файлы</span></a></li>
 		<li class="users"><a href="/admin/users" title="Аккаунты"><span>Аккаунты</span></a></li>
@@ -74,7 +74,7 @@
 <div style="visibility: hidden;display: none;">
 	<!-- @todo: собрать в спрайт -->
 	<img src="/img/admin/dashboard-white.svg" alt="">
-	<img src="/img/admin/blog-white.svg" alt="">
+	<img src="/img/admin/goods-white.svg" alt="">
 	<img src="/img/admin/pages-white.svg" alt="">
 	<img src="/img/admin/files-white.svg" alt="">
 	<img src="/img/admin/users-white.svg" alt="">
